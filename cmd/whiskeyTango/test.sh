46727c4cb392a5b233200727a1032a31612370f4
Robert Fielding <robertfielding@Roberts-Mini.home>
2022-07-14 00:32:40 -0400

    cleanup test

--- cmd/whiskeyTango/test.sh
+++ cmd/whiskeyTango/test.sh
@@ -6,18 +6,20 @@ cd `dirname $0`
 echo --- compile binary
 go build -o wt main.go
 
+echo --- clean up
 rm *.json
+
 echo --- make ca
 ./wt -ca signer.json -kid rfielding-1 -create
-cat signer.json
 
 echo --- trust signer rfielding-1
 ./wt -ca signer.json -kid rfielding-1 -trust trusted.json
+cat trusted.json
 
 echo --- sign token
-echo '{"age":["adult"]}' | ./wt -ca signer.json -kid rfielding-1 -sign > token.json
-cat token.json
+echo '{"age":["adult"]}' | ./wt -ca signer.json -kid rfielding-1 -sign > token.wt
+cat token.wt
 
 echo --- verify token
-cat token.json | ./wt -ca trusted.json -verify
+cat token.wt | ./wt -ca trusted.json -verify
 )
