d7727442a7d2eb736a5c3bb94cdb180ddc2d1e6f
Robert Fielding <robertfielding@Roberts-Mini.home>
2022-07-18 22:15:05 -0400

    colorize test output

--- cmd/whiskeyTango/test.sh
+++ cmd/whiskeyTango/test.sh
@@ -3,6 +3,14 @@
 (
 cd `dirname $0`
 
+# If we don't have jq, then just use cat
+if command -v jq
+then
+  jq=jq
+else
+  jq=cat
+fi
+
 echo --- compile binary
 go build -o wt main.go
 
@@ -10,19 +18,19 @@ echo --- clean up
 rm *.json
 
 echo --- make ca
-./wt -ca signer.json -kid rfielding-1 -create -bits 2048 
+./wt -ca signer.jwk -kid rfielding-1 -create -bits 2048 
 echo
 
 echo --- trust signer rfielding-1
-./wt -ca signer.json -kid rfielding-1 -trust trusted.json
-cat trusted.json
+./wt -ca signer.jwk -kid rfielding-1 -trust trusted.jwk
+cat trusted.jwk | $jq
 echo
 
 echo --- sign token
-echo '{"age":["adult"]}' | ./wt -ca signer.json -kid rfielding-1 -sign > token.wt
+echo '{"age":["adult"]}' | ./wt -ca signer.jwk -kid rfielding-1 -sign > token.wt
 cat token.wt
 echo
 
 echo --- verify token
-cat token.wt | ./wt -ca trusted.json -verify
+cat token.wt | ./wt -ca trusted.jwk -verify | $jq
 )
